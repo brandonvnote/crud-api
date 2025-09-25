844eed9c3ad24411517eec1cedabc372d48f6101
brandonvnote <brandonvnote@gmail.com>
2025-09-25 11:47:20 -0700

    feat: add get all customers endpoint

--- app/routers/customers.py
+++ app/routers/customers.py
@@ -6,9 +6,14 @@ from ..database import get_db
 router = APIRouter(prefix="/customers", tags=["customers"])
 
 @router.post("/", response_model=schemas.CustomerResponse)
+
 def create_customer(customer: schemas.CustomerCreate, db: Session = Depends(get_db)):
     new_customer = models.Customer(**customer.model_dump())
     db.add(new_customer)
     db.commit()
     db.refresh(new_customer)
     return new_customer
+
+@router.get("/", response_model=list[schemas.CustomerResponse])
+def read_customers(db: Session = Depends(get_db)):
+    return db.query(models.Customer).all()
