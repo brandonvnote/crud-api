0d54743ac579cba86b268a82819ee032eb59ee42
brandonvnote <brandonvnote@gmail.com>
2025-09-29 13:34:05 -0700

    chore: register reviews router in main application

--- app/main.py
+++ app/main.py
@@ -2,14 +2,14 @@ from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from sqlalchemy import text
 from app.database import get_db
-from .routers import customers, products, orders
+from .routers import customers, products, orders, reviews
 
 app = FastAPI(title="CRUD API")
 
 app.include_router(customers.router)
 app.include_router(products.router)
 app.include_router(orders.router)
-
+app.include_router(reviews.router)
 
 @app.get("/")
 def root():
